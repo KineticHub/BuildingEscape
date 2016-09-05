cdd1c24c212a0401b0421d115a0ac0472823f297
Kinetic Alnajar <kalnajar09@gmail.com>
2016-09-04 23:20:52 -0400

    intial code source from mac

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,39 @@
+// Copyright ShiftingDiscord 2016.
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+    
+    
+    
+    
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+    if (PressurePlate->IsOverlappingActor(PawnActor)) {
+        AActor* Owner = GetOwner();
+        FRotator NewRotation = FRotator(0.0f, OpenAngle, 0.0f);
+        Owner->SetActorRotation(NewRotation);
+    }
+}
